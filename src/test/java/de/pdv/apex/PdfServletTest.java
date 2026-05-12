2dce327be8e52250004127317f25e29dcc23e7cd
oliver1 <oliver.warz@pdv.de>
2026-05-13 00:26:16 +0200

    Change TransformerFactory

--- src/test/java/de/pdv/apex/PdfServletTest.java
+++ src/test/java/de/pdv/apex/PdfServletTest.java
@@ -41,6 +41,7 @@ class PdfServletTest extends Mockito {
         when(servlet.getServletConfig()).thenReturn(servletConfig);
         when(response.getOutputStream()).thenReturn(outputStream);
         when(response.getWriter()).thenReturn(new PrintWriter(new StringWriter()));
+        servlet.init();
         servlet.doGet(request, response);
         System.out.println("Check for header %PDF-1.4%...");
         byte[] b = {0x25,0x50,0x44,0x46,0x2D,0x31,0x2E,0x34,0x0A}; //
@@ -62,6 +63,7 @@ class PdfServletTest extends Mockito {
         when(request.getParameter("template")).thenReturn(xsltFileContent);
         when(request.getParameter("xml")).thenReturn(xmlFileContent);
         when(response.getWriter()).thenReturn(new PrintWriter(new StringWriter()));
+        servlet.init();
         servlet.doPost(request, response);
         System.out.println("Check for header %PDF-1.4%...");
         byte[] b = {0x25,0x50,0x44,0x46,0x2D,0x31,0x2E,0x34,0x0A}; //
