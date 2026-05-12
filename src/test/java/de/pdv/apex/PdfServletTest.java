0af1ae5bf222be5da5c7a7226b07adbc244be855
oliver1 <oliver.warz@pdv.de>
2026-05-13 01:21:20 +0200

    (fix) Additional tests

--- src/test/java/de/pdv/apex/PdfServletTest.java
+++ src/test/java/de/pdv/apex/PdfServletTest.java
@@ -10,17 +10,22 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class PdfServletTest extends Mockito {
     @Spy  private PdfServlet servlet;
     @Mock private ServletConfig servletConfig;
+    @Mock private ServletContext servletContext;
     @Mock private HttpServletRequest request;
     @Mock private HttpServletResponse response;
     @Mock private ServletOutputStream outputStream;
@@ -67,6 +72,39 @@ class PdfServletTest extends Mockito {
         byte[] b = {0x25,0x50,0x44,0x46,0x2D,0x31,0x2E,0x34,0x0A}; //
         verify(outputStream, atLeastOnce()).write(b);
         System.out.println("Success!");
+    }
+
+    @Test
+    void doPostLogsTransformationErrorWhenParametersAreMissing() throws Exception {
+        Logger logger = Logger.getLogger(PdfServlet.class.getName());
+        Level previousLevel = logger.getLevel();
+        logger.setLevel(Level.FINEST);
+        try {
+            when(servlet.getServletConfig()).thenReturn(servletConfig);
+            when(servlet.getServletContext()).thenReturn(servletContext);
+            when(response.getOutputStream()).thenReturn(outputStream);
+            when(request.getContentType()).thenReturn("text/xml");
+
+            servlet.doPost(request, response);
+
+            verify(request).getParameter("template");
+            verify(request).getParameter("xml");
+            verify(response).setContentType("application/pdf");
+            verify(response).setHeader("Content-disposition", "attachment; filename=Kostenblatt.pdf");
+            verify(servletContext).log(any(), any(Exception.class));
+        } finally {
+            logger.setLevel(previousLevel);
+        }
+    }
+
+    @Test
+    void doGetLogsTransformationErrorWhenOutputStreamFails() throws Exception {
+        when(servlet.getServletConfig()).thenReturn(servletConfig);
+        when(response.getOutputStream()).thenThrow(new IOException("stream unavailable"));
+
+        servlet.doGet(request, response);
+
+        verify(response).setContentType("application/pdf");
     }
 
     @Test
